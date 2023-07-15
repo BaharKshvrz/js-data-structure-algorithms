ec34fdc4b15e13f5bf47974dc591099547ecd51c
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2023-07-15 11:16:11 +0300

    modify the hash table

--- data-structure/hash-table/2.create-own-hash-table.js
+++ data-structure/hash-table/2.create-own-hash-table.js
@@ -47,25 +47,54 @@ class HashTable {
 
 
     _hash( key ) {
+       // select a place for data 
        let hash = 0;
-       for (let i= 0; i < key.length; i++) {
+       for (let i= 0; i < key?.length; i++) {
          hash = (hash + key.charCodeAt(i) * i) % this.data.length;
        }
-
        return hash;
     }
 }
 
 let hashTbl = new HashTable(50);
 hashTbl.set('a', 1000);
+hashTbl.set('a', 101);
 hashTbl.set('b', 10);
 hashTbl.set('c', 150);
 hashTbl.set('d', 77);
 hashTbl.set('e', 99);
 hashTbl.set('f', 25);
+hashTbl.set('ab', 650);
+hashTbl.set('abc', 1000);
+hashTbl.set(null, null);
+hashTbl.set(null, 50);
+hashTbl.set('bb', null);
 console.log("main data:", hashTbl.data);
+/*
+main data: [
+  [
+    [ 'a', 1000 ],
+    [ 'a', 101 ],
+    [ 'b', 10 ],
+    [ 'c', 150 ],
+    [ 'd', 77 ],
+    [ 'e', 99 ],
+    [ 'f', 25 ],
+    [ null, null ],
+    [ null, 50 ]
+  ],
+  <45 empty items>,
+  [ [ 'abc', 1000 ] ],
+  <1 empty item>,
+  [ [ 'ab', 650 ], [ 'bb', null ] ],
+  <1 empty item>
+]
+*/
+
+
+
 
 // console.log(hashTbl.get('a'));
 // console.log(hashTbl.get('b'));
 
-console.log("data keys", hashTbl.keys());
+console.log("data keys:", hashTbl.keys());
